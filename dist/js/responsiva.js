0ea59dddf8192cd5dc3efbbd6bdfe76a6fad172c
Alex Volkov <vol4ikman@gmail.com>
2015-03-19 21:54:00 +0200

    Update responsiva.js

--- dist/js/responsiva.js
+++ dist/js/responsiva.js
@@ -1,3 +1,19 @@
 jQuery(document).ready(function(){
 
+	//Responsiva Accordion
+	jQuery(".raccardion_title").click(function(e){
+		e.preventDefault();
+		var title = jQuery(this);
+		var content = title.next(".raccardion_content");
+		title.toggleClass("active");
+		if(title.is(".active")) {
+			console.log("i am active");
+			jQuery(".raccardion_content").slideUp(250);
+		} else {
+			var content = title.next(".raccardion_content");
+			content.slideDown(250);	
+		}
+		content.slideToggle(300);
+	});
+
 })
